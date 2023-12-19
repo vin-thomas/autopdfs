4f6cbf8c652b2ad45a1cd0039e8a397676d2e473
tnuocca wen <benshalomalbert@gmail.com>
2023-12-19 13:37:03 +0530

    latest

--- find.py
+++ find.py
@@ -19,24 +19,12 @@ try:
     driver.get(url)
     time.sleep(5)  # Adjust this time according to the page load speed
 
-    # Perform Ctrl + F and search for the phrase
-    # search_phrase = "Earnings Call Transcript"
-    # body = driver.find_element(By.TAG_NAME, "body")
-    # body.send_keys(Keys.CONTROL + 'f')  # Opens the browser's find box
-    
+    elems = driver.find_elements(By.XPATH, "//*[contains(text(), 'Earnings Call Transcript')]")
+    for elem in elems:
+        print(elem.get_attribute('innerText'))
+        welem = driver.find_element(locate_with(By.TAG_NAME, 'a').to_right_of(elem))
+        print(welem.get_attribute('href'))
 
-    elem = driver.find_element(By.XPATH, "//*[contains(text(), 'Earnings Call Transcript')]")
-    print(elem.get_attribute('innerText'))
-
-    welem = driver.find_element(locate_with(By.TAG_NAME, 'a').to_right_of(elem))
-    print(welem.get_attribute('href'))
-
-    # Type the search phrase in the find box
-    # time.sleep(1)  # Adding a delay for the find box to open
-    # body.send_keys(search_phrase)
-    # time.sleep(2)  # Adjust this time according to the search speed
-
-    # Close the browser
     driver.quit()
 
 except Exception as e:
