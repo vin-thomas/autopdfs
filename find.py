60ad3393d835f511e4e6177aaa439c1b4633dbc4
tnuocca wen <benshalomalbert@gmail.com>
2023-12-20 12:19:08 +0530

    pdf from screener too

--- find.py
+++ find.py
@@ -13,11 +13,9 @@ import re
 driver = webdriver.Chrome()
 
 
-def company_urls():
+def bse_urls():
     df = pd.read_csv("Equity.csv")
     df = df[["Security Code", "Security Id", "Security Name"]]
-    c = 0
-    print(len(df))
     urls = []
     for i in df.iterrows():
         code = i[1][0]
@@ -31,28 +29,107 @@ def company_urls():
         urls.append(f'https://www.bseindia.com/stock-share-price/{name}/{id}/{code}/corp-announcements/')
     return urls
 
-def get_pdf_links():
-    urls = company_urls()
+
+def screener_urls():
+    df = pd.read_csv("Equity.csv")
+    df = df["Security Id"]
+    urls = []
+    for i in df.tolist():
+        urls.append(f'https://www.screener.in/company/{i}/')
+    return urls
+
+
+def order_urls(urls, ch):
+    df = pd.read_csv('ind_nifty50list.csv')
+    df = df['Symbol']
+    df = df.sort_values(ascending=False)        
+    for i in range(len(urls)):
+        for sym in df.tolist():
+            if ch == 1:
+                if urls[i].split('/')[-4].upper() == sym:
+                    urls.insert(0, urls.pop(i))
+            elif ch == 2:
+                if urls[i].split('/')[-2] == sym:
+                    urls.insert(0, urls.pop(i))
+    return urls
+
+
+def from_bse(urls):
+    id = []
+    pdf_list = []
     c = 0
     for url in urls:
-        if c < 10:
+        if c < 50:
             try:
                 # Open the webpage
                 driver.get(url)
-                time.sleep(5)  # Adjust this time according to the page load speed
+                time.sleep(2)  # Adjust this time according to the page load speed
 
                 elems = driver.find_elements(By.XPATH, "//*[contains(text(), 'Earnings Call Transcript')]")
+                href = []
                 for elem in elems:
                     print(elem.get_attribute('innerText'))
                     welem = driver.find_element(locate_with(By.TAG_NAME, 'a').to_right_of(elem))
-                    print(welem.get_attribute('href'))
-
+                    href.append(welem.get_attribute('href') )
+                    print(href)
+                id.append(url.split('/')[-4].upper())
+                pdf_list.append(href)
 
             except Exception as e:
                 print("An error occurred:", e)
                 continue
         c += 1
     driver.quit()
+    return id, pdf_list
 
-if __name__== "__main__":
+
+def from_screener(urls):
+    id = []
+    pdf_list = []
+    c = 0
+    for url in urls:
+        if c < 50:
+            try:
+                # Open the webpage
+                driver.get(url)
+                time.sleep(2)  # Adjust this time according to the page load speed
+
+                elems = driver.find_elements(By.XPATH, "//a[contains(@class, 'concall-link') and contains(text(), 'Transcript')]")
+                href = []
+                for elem in elems:
+                    print(elem.get_attribute('innerText'))
+                    href.append(elem.get_attribute('href'))
+                print(href)
+                id.append(url.split('/')[-2].upper())
+                pdf_list.append(href)
+
+            except Exception as e:
+                print("An error occurred:", e)
+                continue
+        c += 1
+    driver.quit()
+    return id, pdf_list
+
+
+def get_pdf_links():
+    # urlsb = order_urls(bse_urls(), 1)
+    # id, pdf_list = from_bse(urlsb)
+
+    # df = pd.DataFrame()
+    # df['id'] = id
+    # df["pdfs"] = pdf_list
+    # df.to_csv("pdf_list.csv", index=False)
+
+
+    urlss = order_urls(screener_urls(), 2)
+    sid, spdf_list = from_screener(urlss)
+
+    sdf = pd.DataFrame()
+    sdf['id'] = sid
+    sdf["pdfs"] = spdf_list
+    sdf.to_csv("spdf_list.csv", index=False)
+
+    
+
+if __name__ == "__main__":
     get_pdf_links()
